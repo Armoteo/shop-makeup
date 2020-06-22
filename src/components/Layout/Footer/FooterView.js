e69263aee384f110c52a57026debab9d036b1a5c
Viktor Korotkiy <55664118+Armoteo@users.noreply.github.com>
2020-06-22 08:42:59 +0300

    foter ready

--- src/components/Layout/Footer/FooterView.js
+++ src/components/Layout/Footer/FooterView.js
@@ -3,10 +3,12 @@ import React from 'react'
 import styles from '../../../sass/components/layout/Footer.module.scss'
 import Copyright from './components/Copyright.js'
 import FooterNav from './components/FooterNav'
+import FooterInfo from './components/FooterInfo'
 
 const FooterView = () => {
   return (
     <div className={styles.Footer}>
+      <FooterInfo />
       <FooterNav />
       <Copyright />
     </div>
--- src/components/Layout/Footer/components/FooterInfo.js
+++ src/components/Layout/Footer/components/FooterInfo.js
@@ -1,1 +1,38 @@
 import React from 'react'
+
+import styles from '../../../../sass/components/elements/FooterInfo.module.scss'
+
+const FooterInfo = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapperContent}>
+        <div>
+          <h3>Suspendisse Sed</h3>
+          <span>
+            Lectus magna fringilla urna porttitor rhoncus dolor purus non.
+            Semper viverra nam libero justo laoreet sit. Dolor purus non enim
+            praesent elementum facilisis leo vel.
+          </span>
+        </div>
+        <div>
+          <h3>Suspendisse Sed</h3>
+          <span>
+            Lectus magna fringilla urna porttitor rhoncus dolor purus non.
+            Semper viverra nam libero justo laoreet sit. Dolor purus non enim
+            praesent elementum facilisis leo vel.
+          </span>
+        </div>
+        <div>
+          <h3>Suspendisse Sed</h3>
+          <span>
+            Lectus magna fringilla urna porttitor rhoncus dolor purus non.
+            Semper viverra nam libero justo laoreet sit. Dolor purus non enim
+            praesent elementum facilisis leo vel.
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FooterInfo
